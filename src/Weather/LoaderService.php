f15715d7a67d494d6308249999179b9bdf7f3771
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2018-11-19 13:52:36 +0200

    we want to implement caching, so lets add FilesystemCache service into our Loader service.
    WE should get an error:
    
    Cannot resolve argument $weatherLoaderService of "App\Controller\WeatherController::index()": Cannot autowire service "App\Weather\LoaderService": argument "$cacheService" of method "__construct()" references class "Symfony\Component\Cache\Simple\FilesystemCache" but no such service exists. Try changing the type-hint to "Psr\SimpleCache\CacheInterface" instead.

--- src/Weather/LoaderService.php
+++ src/Weather/LoaderService.php
@@ -4,6 +4,7 @@ namespace App\Weather;
 
 use App\GoogleApi\WeatherService;
 use App\Model\Weather;
+use Symfony\Component\Cache\Simple\FilesystemCache;
 
 class LoaderService
 {
@@ -11,13 +12,17 @@ class LoaderService
     /** @var WeatherService */
     private $googleWeatherService;
 
+    /** @var FilesystemCache */
+    private $cacheService;
+
     /**
      * LoaderService constructor.
      * @param WeatherService $googleWeatherService
      */
-    public function __construct(WeatherService $googleWeatherService)
+    public function __construct(WeatherService $googleWeatherService, FilesystemCache $cacheService)
     {
         $this->googleWeatherService = $googleWeatherService;
+        $this->cacheService = $cacheService;
     }
 
     /**
