ddf34fceb94e9a44d3ea716f7b78e95bb2647a48
Vaidas A <vaidastr@gmail.com>
2018-11-25 11:39:47 +0200

    Using autowiring. Cleanup.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,20 +9,6 @@ class HomeController extends Controller
 {
 
     /**
-     * @Route("/test", name="test")
-     */
-    public function test()
-    {
-        $now = new \DateTime('now');
-        $birth = new \DateTime('1981-11-20');
-        
-        $diff = date_diff($now, $birth);
-        
-        var_dump($diff->y); die;
-    }
-    
-    
-    /**
      * @Route("/", name="home")
      */
     public function index()
